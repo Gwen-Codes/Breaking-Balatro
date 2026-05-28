405e366baf765a3a5508e3c2371ca372165aa349
Gwen-Codes <GianniAndrada22@outlook.com>
2026-05-28 17:47:28 -0400

    Added Chemist

---
+++ localization/en-us.lua
@@ -0,0 +1,15 @@
+return {
+    descriptions = {
+        Joker = {
+            j_meth_chemist = {
+                name = 'The Chemist',
+                text = {
+                    'Gains {C:chips}+5 {}Chips for',
+                    'every {C:attention}Card {}sold',
+                    'throughout the run',
+                    '{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)'
+                }
+            }
+        }
+    }
+}
